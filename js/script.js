2943a2d821d666370fc6ddcbabcf44db927926ee
Fride <fridebtelle@gmail.com>
2020-05-24 14:57:36 +0200

    script.js

---
+++ js/script.js
@@ -0,0 +1,54 @@
+
+// question 1
+
+var firstVar;
+
+
+
+// question 2
+
+var name = "Fride";
+
+console.log(name);
+
+
+
+// question 3
+
+var integer = 5;
+
+
+
+// question 4
+
+var division = 20 / 5;
+
+
+
+// question 5
+
+var animal = "frog";
+
+typeof(animal);
+
+
+
+// question 6
+
+var orderHasShipped = true;
+
+
+
+// question 7
+
+var orderHasShipped = true;
+
+if(orderHasShipped === true) {
+    console.log("The order has shipped");
+}
+
+// question 8
+
+for(var count = 1; count <= 9; count++) {
+    console.log(count);
+}
